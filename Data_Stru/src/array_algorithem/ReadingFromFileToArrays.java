b2e93e2bc9cbeecff9ff5053046c860d0ce03e8b
yufliu21 <yufliu21@XHNGR2-1042690.ds1.prod>
2016-07-18 10:43:09 +1200

    commit

--- Data_Stru/src/array_algorithem/ReadingFromFileToArrays.java
+++ Data_Stru/src/array_algorithem/ReadingFromFileToArrays.java
@@ -4,20 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+
 public class ReadingFromFileToArrays {
 
 	public static void main(String[] args) {
-		System.out.println("Hi - Wellcome to week 2");
 		int[] myIntArray = new int[10];
 		String[] myStringArray = new String[10];
+		System.out.println("Hi - Wellcome to week 2");
 
-		File file = new File(System.getProperty("user.dir")+"file1.txt");
+		File file = new File(System.getProperty("user.dir") + "file1.txt");
 		try {
 			Scanner scanner = new Scanner(file);
-			for(int i=0;i<=9;i++){
+			for (int i = 0; i <= 9; i++) {
 				myStringArray[i] = scanner.next();
 			}
-			for(int i=0;i<=9;i++){
+			for (int i = 0; i <= 9; i++) {
 				myIntArray[i] = scanner.nextInt();
 			}
 			System.out.println("I am here 1");
@@ -30,7 +31,7 @@ public class ReadingFromFileToArrays {
 			System.out.println("I am here 2");
 
 		} catch (IOException e) {
-			System.out.println("Unable to create: "+e.getMessage());
+			System.out.println("Unable to create: " + e.getMessage());
 		}
 	}
 }
