5499d48534a543dffd7223e622e74b9bc34091a9
liuyufei <gabriel.liu@centling.com>
2017-02-24 22:42:46 +1300

    professional programming

--- Profes_Prog_Prac/src/six_problems/Prog3.scala
+++ Profes_Prog_Prac/src/six_problems/Prog3.scala
@@ -3,37 +3,45 @@ package six_problems
 /**
   * Created by liuyufei on 18/02/17.
   */
+
+class BankAccount{
+
+  private var balance = 0
+
+  //synchronized
+  def deposit(amount:Int):Unit = this.synchronized{
+    if(amount>0) balance+=amount
+  }
+
+  //synchronized
+  def withdraw(amount:Int):Int = this.synchronized{
+    if(amount>0&&balance>=amount){
+      //current thread sleep for 1 second to simulate dirty-read in multithreading
+      Thread.sleep(1000)
+      balance-=amount
+      balance
+    }else{
+      throw new Exception("insufficient funds")
+    }
+  }
+}
+
+class MyThread(var bankAccount: BankAccount) extends Runnable{
+  override def run(): Unit = {
+    println(s"in ${Thread.currentThread().getName}, after withdraw 100, balance is ${bankAccount.withdraw(100)}")
+  }
+}
+
+
 object Prog3 extends App {
 
+  val bankAccount = new BankAccount
+  println("deposit 100 to account")
+  bankAccount.deposit(100)
 
+  //start two thread, in normal case, the second thread should throw "insufficient funds error"
+  new Thread(new MyThread(bankAccount)).start()
+  new Thread(new MyThread(bankAccount)).start()
 
-//
-//  class Bar(val x:Int) {
-//    def this(x: Int)(f: Int => Unit) {
-//      this()
-//    }
-//  }
-//
-//  val b = new Bar(1)(_ + 1)
-//
-//
-//  println(b.x)
-
-  //  case class Foo(var x:Int = 100){
-  //    def plusOne = x+=1
-  //
-  //    def x_= (xxx:Int){
-  //      x = xxx
-  //    }
-  //  }
-  //  val f = Foo()
-  //  f.x = 10
-  //  println(f.x)
-  //
-  //  new Thread(){
-  //    0 to 10 foreach(i=>{
-  //      Foo().plusOne
-  //    })
-  //  }.start()
 
 }
